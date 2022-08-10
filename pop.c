4b1a42e13ef8d2fbcf9a8f3c07ddbfff99ee65ff
JacobM311 <jacobmarshall9608@yahoo.com>
2022-08-10 14:56:43 -0700

    commit

---
+++ pop.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void pop(stack_t **stack, unsigned int line_num)
+{
+	stack_t *new;
+
+	if (!*stack)
+	{
+		printf(stderr, "L%u: can't pop an empty stack\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	new = stack->next;
+	free(stack);
+	stack = new;
+}
