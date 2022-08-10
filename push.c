1a5754f199e33334e058922dc341397abcb372c0
TJonCanon <4923@holbertonstudents.com>
2022-08-10 13:17:45 -0500

    added value for %d

--- push.c
+++ push.c
@@ -8,7 +8,7 @@ void(stack_t **stack, unsigned int line_number)
 {
     if (!stack || !*stack)
     {
-        printf(stderr, "L%u: can't pint, stack empty")
+        printf(stderr, "L%u: can't pint, stack empty", line_number)
         exit(EXIT_FAILTURE);
     }
     printf("%d\n", (*stack)->n);
