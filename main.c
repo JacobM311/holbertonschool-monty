4e314df5ba00e4ba22153e58bd7ab2672ec69d05
TJonCanon <taylor.joyner93@me.com>
2022-08-10 18:37:58 -0500

    Added error cased

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
 #include "main.h"
+int main(int argc, char **argv)
+{
+    FILE *oc;
 
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
+    oc = fopen(argv[1], "r");
+
+    if (!oc)
+    {
+        fprintf(stderr, "Error: Unable open file\n");
+        exit(EXIT_FAILURE);
+    }
+}
