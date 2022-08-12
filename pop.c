9924c344d33bc035f4340e5bdc07d70032cb9270
JacobM311 <jacobmarshall9608@yahoo.com>
2022-08-11 17:13:00 -0700

    commit

--- pop.c
+++ pop.c
@@ -8,15 +8,25 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new;
+		stack_t *hide;
 
-	if (!*stack)
+	if (!*stack)/*there is no stack*/
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
+		op[3] = "ERROR";
+		return;
 	}
-
-	new = (*stack)->next;
-	free(stack);
-	*stack = new;
+	hide = *stack;
+	while (hide->prev)/*find the top*/
+		hide = hide->prev;
+	if (hide->next) /*makes next node top*/
+		hide->next->prev = NULL;
+	if (hide == *stack)
+	{/*makes sure we retain access to list*/
+		if ((*stack)->next)/*there is a second node*/
+			*stack = (*stack)->next;
+		else/*there are no more nodes*/
+			*stack = NULL;
+	}
+	free(hide);/*free the top node*/
 }
